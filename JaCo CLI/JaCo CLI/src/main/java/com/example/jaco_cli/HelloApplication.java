abb919d3adb9c034d6f4a08df8c8b86874370c27
Harsh Pathak <83153262+iharshpathak@users.noreply.github.com>
2022-05-18 08:07:34 +0530

    GUI v2.1.O, added app icon
    Added App Icon

--- JaCo CLI/JaCo CLI/src/main/java/com/example/jaco_cli/HelloApplication.java
+++ JaCo CLI/JaCo CLI/src/main/java/com/example/jaco_cli/HelloApplication.java
@@ -5,9 +5,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.DataInput;
 import java.io.IOException;
@@ -22,6 +24,8 @@ public class HelloApplication extends Application {
         scene.setFill(Color.DARKSLATEGREY);
         stage.setFullScreen(false);
         stage.setResizable(false);
+        stage.initStyle(StageStyle.DECORATED);
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/Images/JaCoIcon.png")));
         stage.setScene(scene);
         stage.show();
        // new DataProviderService().getData("India");
